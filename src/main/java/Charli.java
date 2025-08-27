66e1d0bc9319dcda3b73f951826c4008c246bf8d
Valerie <valeriexylim.sg@gmail.com>
2025-08-27 16:06:02 +0800

    Level-1: Implement echo functionality with proper formatting

--- src/main/java/Charli.java
+++ src/main/java/Charli.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+
 public class Charli {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         String logo =
                 "         ,--./,-.   \n"
             + "        / #      \\  \n"
@@ -7,8 +11,25 @@ public class Charli {
             + "        \\        /  \n"
             + "         `._,._.'   \n";
 
-        System.out.println("Hi British Vogue, I'm CharliXCX and this is what's in my bag!!!!\nXOXO\n" + logo);
+        System.out.println("Hi British Vogue, I'm CharliXCX!\nWhat club classics would you like on your summer rotation?\n");
+        String input;
 
+        //read input until the input is "bye"
+        do {
+            //System.out.print("    "); // Indentation for user input
+            input = scanner.nextLine();
+
+            System.out.println("    ____________________________________________________________");
+
+            if (!input.equals("bye")) {
+                System.out.println("    " + input);
+            } else {
+                System.out.println("    XOXO\n" + logo);
+            }
+
+        } while (!input.equals("bye"));
+
+        scanner.close();
     }
 }
 
