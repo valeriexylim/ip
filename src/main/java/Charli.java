f1a7303b2a8073f6890429f60214400312b417f6
Valerie <valeriexylim.sg@gmail.com>
2025-08-27 15:36:36 +0800

    Level 0: Rename bot to Charli and add greetings

---
+++ src/main/java/Charli.java
@@ -0,0 +1,15 @@
+public class Charli {
+    public static void main(String[] args) {
+        String logo =
+                "         ,--./,-.   \n"
+            + "        / #      \\  \n"
+            + "       |          | \n"
+            + "        \\        /  \n"
+            + "         `._,._.'   \n";
+
+        System.out.println("Hi British Vogue, I'm CharliXCX and this is what's in my bag!!!!\nXOXO\n" + logo);
+
+    }
+}
+
+
