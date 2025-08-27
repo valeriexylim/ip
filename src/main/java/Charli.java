9dc9a80ce9d83226f3d223ddb7a7cc06a5176767
Valerie <valeriexylim.sg@gmail.com>
2025-08-27 16:30:14 +0800

    Level-2: Add task and listing functionality

--- src/main/java/Charli.java
+++ src/main/java/Charli.java
@@ -3,7 +3,8 @@ import java.util.Scanner;
 public class Charli {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        String[] mix = new String[100];
+        int count = 0;
         String logo =
                 "         ,--./,-.   \n"
             + "        / #      \\  \n"
@@ -21,12 +22,22 @@ public class Charli {
 
             System.out.println("    ____________________________________________________________");
 
-            if (!input.equals("bye")) {
-                System.out.println("    " + input);
-            } else {
+            if(input.equals("rotation")) {
+                if (count == 0) {
+                    System.out.println("    YUCK No songs yet!!\n" + logo);
+                } else {
+                    System.out.println("    INCREDIBLE MIX INCOMING!!!\n");
+                    for (int i = 0; i < count; i++) {
+                        System.out.println("      " + (i + 1) + ". " + mix[i] + "\n");
+                    }
+                }
+            } else if (input.equals("bye")) {
                 System.out.println("    XOXO\n" + logo);
+            } else {
+                mix[count] = input;
+                count++;
+                System.out.println("    added: " + input);
             }
-
         } while (!input.equals("bye"));
 
         scanner.close();
