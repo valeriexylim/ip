7b51b307f33fc00148707039114bdb04ef224963
Valerie <valeriexylim.sg@gmail.com>
2025-08-28 14:04:15 +0800

    Add getter for description to Task class

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -7,6 +7,10 @@ public class Task {
         this.isDone = false;
     }
 
+    public getDescription() {
+        return description;
+    }
+
     public String getStatusIcon() {
         return (isDone ? "✓" : " ");
     }
