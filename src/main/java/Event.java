61b2ccffdc4552209e2156ee9b7c3a78e5ca5d02
Valerie <valeriexylim.sg@gmail.com>
2025-09-02 23:56:16 +0800

    Add getters for from and to

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -12,4 +12,11 @@ public class Event extends Task {
     public String toString() {
         return "[E]" + super.toString() + " (from: " + from + " to: " + to + ")";
     }
+
+    public String getFrom() {
+        return from;
+    }
+    public String getTo() {
+        return to;
+    }
 }
