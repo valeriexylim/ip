ac67933bb088abc7bc7887b9e56f9da810eb1b0a
Valerie <valeriexylim.sg@gmail.com>
2025-09-09 18:16:48 +0800

    feat(Level-9): Add FindCommand class and update Parser to include FindCommand

--- src/main/java/charli/util/Parser.java
+++ src/main/java/charli/util/Parser.java
@@ -1,5 +1,11 @@
 package charli.util;
 import charli.command.Command;
+<<<<<<< Updated upstream
+=======
+import charli.command.DeleteCommand;
+import charli.command.ExitCommand;
+import charli.command.FindCommand;
+>>>>>>> Stashed changes
 import charli.command.ListCommand;
 import charli.command.MarkCommand;
 import charli.command.AddTodoCommand;
@@ -17,16 +23,18 @@ public class Parser {
             return new MarkCommand(fullCommand,true);
         } else if (fullCommand.startsWith("unplayed ")) {
             return new MarkCommand(fullCommand, false);
-        } else if (fullCommand.startsWith("bop ")) {  //Changed from "todo"
+        } else if (fullCommand.startsWith("bop ")) {  // Changed from "todo"
             return new AddTodoCommand(fullCommand);
-        } else if (fullCommand.startsWith("drop ")) {  //Changed from "deadline"
+        } else if (fullCommand.startsWith("drop ")) {  // Changed from "deadline"
             return new AddDeadlineCommand(fullCommand);
-        } else if (fullCommand.startsWith("show ")) {  //Changed from "event"
+        } else if (fullCommand.startsWith("show ")) {  // Changed from "event"
             return new AddEventCommand(fullCommand);
         } else if (fullCommand.startsWith("delete ")) {
             return new DeleteCommand(fullCommand);
         } else if (fullCommand.equals("bye")) {
             return new ExitCommand();
+        } else if (fullCommand.startsWith("find ")) {
+            return new FindCommand(fullCommand);
         } else {
             throw new CharliException("Use: bop [song], drop [song] /by [date], or show [event] /from [time] /to [time]");
         }
