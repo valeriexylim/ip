c6e05293646ae7f441b5e7f1248fca80ccc8c659
Valerie <valeriexylim.sg@gmail.com>
2025-09-15 16:20:28 +0800

    Add deadline after current time test

--- src/test/java/charli/task/DeadlineTest.java
+++ src/test/java/charli/task/DeadlineTest.java
@@ -1,5 +1,7 @@
 package charli.task;
 
+import charli.exception.CharliException;
+
 import java.time.LocalDateTime;
 import java.time.format.DateTimeParseException;
 
@@ -13,24 +15,24 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class DeadlineTest {
 
     @Test
-    public void testDeadlineCreation() {
-        Deadline deadline = new Deadline("Release album", "25/12/2024 1800");
+    public void testDeadlineCreation() throws CharliException {
+        Deadline deadline = new Deadline("Release album", "25/12/2029 1800");
         assertEquals("Release album", deadline.getDescription());
         assertFalse(deadline.isDone());
         assertTrue(deadline.toString().contains("Release album"));
-        assertTrue(deadline.toString().contains("Dec 25 2024, 6:00pm"));
+        assertTrue(deadline.toString().contains("Dec 25 2029, 6:00pm"));
     }
 
     @Test
     public void testDeadlineWithLocalDateTime() {
-        LocalDateTime by = LocalDateTime.of(2024, 12, 25, 18, 0);
+        LocalDateTime by = LocalDateTime.of(2029, 12, 25, 18, 0);
         Deadline deadline = new Deadline("Release album", by);
         assertEquals(by, deadline.getBy());
     }
 
     @Test
-    public void testDeadlineMarkAsDone() {
-        Deadline deadline = new Deadline("Release album", "25/12/2024 1800");
+    public void testDeadlineMarkAsDone() throws CharliException {
+        Deadline deadline = new Deadline("Release album", "25/12/2029 1800");
         deadline.markAsDone();
         assertTrue(deadline.isDone());
         assertTrue(deadline.toString().contains("[✓]"));
